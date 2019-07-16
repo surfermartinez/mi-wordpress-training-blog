ab44634843fe554cf0b454ff73819856f8f0d80f
Fernando Martinez <fernandomartinez@Fernandos-MacBook-Pro.local>
2019-07-16 18:41:58 +1000

    start of the blog

---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php 
+
+// show featured image in the post admin screen
+add_theme_support( 'post-thumbnails' );
---
+++ index.php
@@ -0,0 +1,34 @@
+
+<head> 
+    <title>Lino fun | All you need to know about Lino printing</title>
+</head>
+
+<body>
+    <h1>Latest Posts</h1>
+    <ul>
+        <li>
+            <article class="card">
+                <h2>Post Title</h2>
+                <img src="http://placehold.it/300x200">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet consectetur felis, eget rhoncus erat. Praesent convallis facilisis sodales. Integer metus ex, posuere molestie ultrices a, pulvinar vitae eros. Proin dui eros, rhoncus vitae nisl accumsan, ultricies tristique mauris. In vel lectus et metus sodales rutrum. Phasellus eleifend mollis dui, sed ultrices leo pretium in. Vestibulum rutrum auctor erat in ornare. Nam ullamcorper eu quam in aliquet. Cras in egestas leo. Nulla facilisi. Pellentesque et augue sit amet dui aliquet egestas quis ut nibh. Mauris laoreet turpis at viverra commodo. Maecenas at ipsum in leo porttitor hendrerit. Phasellus scelerisque ullamcorper ligula, sed venenatis lectus vulputate in. Vestibulum efficitur ipsum id vestibulum blandit.</p>
+                <a>Read More</a>
+            </article>
+        </li>
+         <li>
+           <article class="card">
+                <h2>Post Title</h2>
+                <img src="http://placehold.it/300x200">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet consectetur felis, eget rhoncus erat. Praesent convallis facilisis sodales. Integer metus ex, posuere molestie ultrices a, pulvinar vitae eros. Proin dui eros, rhoncus vitae nisl accumsan, ultricies tristique mauris. In vel lectus et metus sodales rutrum. Phasellus eleifend mollis dui, sed ultrices leo pretium in. Vestibulum rutrum auctor erat in ornare. Nam ullamcorper eu quam in aliquet. Cras in egestas leo. Nulla facilisi. Pellentesque et augue sit amet dui aliquet egestas quis ut nibh. Mauris laoreet turpis at viverra commodo. Maecenas at ipsum in leo porttitor hendrerit. Phasellus scelerisque ullamcorper ligula, sed venenatis lectus vulputate in. Vestibulum efficitur ipsum id vestibulum blandit.</p>
+                <a>Read More</a>
+            </article>
+        </li>
+         <li>
+            <article class="card">
+                <h2>Post Title</h2>
+                <img src="http://placehold.it/300x200">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet consectetur felis, eget rhoncus erat. Praesent convallis facilisis sodales. Integer metus ex, posuere molestie ultrices a, pulvinar vitae eros. Proin dui eros, rhoncus vitae nisl accumsan, ultricies tristique mauris. In vel lectus et metus sodales rutrum. Phasellus eleifend mollis dui, sed ultrices leo pretium in. Vestibulum rutrum auctor erat in ornare. Nam ullamcorper eu quam in aliquet. Cras in egestas leo. Nulla facilisi. Pellentesque et augue sit amet dui aliquet egestas quis ut nibh. Mauris laoreet turpis at viverra commodo. Maecenas at ipsum in leo porttitor hendrerit. Phasellus scelerisque ullamcorper ligula, sed venenatis lectus vulputate in. Vestibulum efficitur ipsum id vestibulum blandit.</p>
+                <a>Read More</a>
+            </article>
+        </li>
+    </ul>
+</body>
