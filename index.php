0d972677a91c7d12d3011336ca988ddd1c2b4f86
Fernando Martinez <fernandomartinez@Fernandos-MacBook-Pro.local>
2019-07-17 19:53:10 +1000

    restart the css and use bootstrap

--- index.php
+++ index.php
@@ -1,34 +1,15 @@
-
 <head> 
     <title>Lino fun | All you need to know about Lino printing</title>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
     <h1>Latest Posts</h1>
-    <ul>
-        <li>
-            <article class="card">
-                <h2>Post Title</h2>
-                <img src="http://placehold.it/300x200">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet consectetur felis, eget rhoncus erat. Praesent convallis facilisis sodales. Integer metus ex, posuere molestie ultrices a, pulvinar vitae eros. Proin dui eros, rhoncus vitae nisl accumsan, ultricies tristique mauris. In vel lectus et metus sodales rutrum. Phasellus eleifend mollis dui, sed ultrices leo pretium in. Vestibulum rutrum auctor erat in ornare. Nam ullamcorper eu quam in aliquet. Cras in egestas leo. Nulla facilisi. Pellentesque et augue sit amet dui aliquet egestas quis ut nibh. Mauris laoreet turpis at viverra commodo. Maecenas at ipsum in leo porttitor hendrerit. Phasellus scelerisque ullamcorper ligula, sed venenatis lectus vulputate in. Vestibulum efficitur ipsum id vestibulum blandit.</p>
-                <a>Read More</a>
-            </article>
-        </li>
-         <li>
-           <article class="card">
-                <h2>Post Title</h2>
-                <img src="http://placehold.it/300x200">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet consectetur felis, eget rhoncus erat. Praesent convallis facilisis sodales. Integer metus ex, posuere molestie ultrices a, pulvinar vitae eros. Proin dui eros, rhoncus vitae nisl accumsan, ultricies tristique mauris. In vel lectus et metus sodales rutrum. Phasellus eleifend mollis dui, sed ultrices leo pretium in. Vestibulum rutrum auctor erat in ornare. Nam ullamcorper eu quam in aliquet. Cras in egestas leo. Nulla facilisi. Pellentesque et augue sit amet dui aliquet egestas quis ut nibh. Mauris laoreet turpis at viverra commodo. Maecenas at ipsum in leo porttitor hendrerit. Phasellus scelerisque ullamcorper ligula, sed venenatis lectus vulputate in. Vestibulum efficitur ipsum id vestibulum blandit.</p>
-                <a>Read More</a>
-            </article>
-        </li>
-         <li>
-            <article class="card">
-                <h2>Post Title</h2>
-                <img src="http://placehold.it/300x200">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet consectetur felis, eget rhoncus erat. Praesent convallis facilisis sodales. Integer metus ex, posuere molestie ultrices a, pulvinar vitae eros. Proin dui eros, rhoncus vitae nisl accumsan, ultricies tristique mauris. In vel lectus et metus sodales rutrum. Phasellus eleifend mollis dui, sed ultrices leo pretium in. Vestibulum rutrum auctor erat in ornare. Nam ullamcorper eu quam in aliquet. Cras in egestas leo. Nulla facilisi. Pellentesque et augue sit amet dui aliquet egestas quis ut nibh. Mauris laoreet turpis at viverra commodo. Maecenas at ipsum in leo porttitor hendrerit. Phasellus scelerisque ullamcorper ligula, sed venenatis lectus vulputate in. Vestibulum efficitur ipsum id vestibulum blandit.</p>
-                <a>Read More</a>
-            </article>
-        </li>
+
+    <?php $x = 1; while($x <= 12) { ?>
+
+
+    <?php $x++; } ?>
+
     </ul>
 </body>
